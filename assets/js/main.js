36a1a61b0adc83e3021ee3c1e123344cc4a1cdaf
JohanBermeo <sonjohan@hotmail.com>
2018-06-28 13:08:59 -0400

    front end completed

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    var config = {
+        apiKey: "AIzaSyDobIQNlWrizZRoHEDQrr2qcRWOGHFI4BY",
+        authDomain: "train-scheduler-acf86.firebaseapp.com",
+        databaseURL: "https://train-scheduler-acf86.firebaseio.com",
+        projectId: "train-scheduler-acf86",
+        storageBucket: "",
+        messagingSenderId: "871984040375"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+});
